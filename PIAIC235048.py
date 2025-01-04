531dd4260e6182d44dceafdfdbf009d295adea14
WAFA <wafadar801@gmail.com>
2025-01-05 03:30:43 +0500

    First github repo with python

---
+++ PIAIC235048.py
@@ -0,0 +1,55 @@
+Exercise_3-1:Names
+
+names:list=['john','Jane','Michael']
+print(f"Friends' names list: {names}")
+print(names[0])
+print(names[1])
+print(names[2])
+print('.')
+print('.')
+print('.')
+
+
+#Exercise_3-2:Greetings
+
+print(f'Hello,{names[0]}! How are you doing?')
+print(f'Hello,{names[1]}! How are you doing?')
+print(f'Hello,{names[2]}! How are you doing?')
+print('.')
+print('.')
+print('.')
+
+
+#Exercise_3-3:My Own list
+
+Favourite_Transportation_Mode:list=['AIRBUS ACH130 HELICOPTER ASTON MARTIN EDITION','2024 OCEANCO Y1050',
+'JEEP WRANGLER RUBICON 392 FINAL EDITION','BMW R18']
+print(f'Names List of Favourite transportation Modes: {Favourite_Transportation_Mode}')
+print('.')
+print(f'Imagine {Favourite_Transportation_Mode[0]} soaring through the skies in this luxurious and stylish helicopters!')
+print(f'Cruising {Favourite_Transportation_Mode[1]}the open seas on this magnificent yacht would be an unforgettable experience')
+print(f'This rugged{Favourite_Transportation_Mode[2]} powerful off-road vehicle is perfect for adventure')
+print(f'This{Favourite_Transportation_Mode[3]}classic and stylish motorcycle is a dream to ride')
+print('.')
+print('.')
+print('.')
+
+
+#Exercise 3-4: Guest List
+
+Guest_list:list=['Imran Ahmed Khan Niazi','Dr.Javed Ahmed','Sayyada Tul Hurrah','Nelson Mandela','Nicola Tesla','Marylin Monroe','Greyson Brokk']
+print(f"Guests' names list for dinner invitation: {Guest_list}")
+print('.')
+print(f'Assalamu Alaikum Wa RahmatUllah Hi Wa Barakatuhu, Respected, Mr.{Guest_list[0]}! I am very delighted to invite you on a dinner at my place, I would be really delighted if u accept my mere invitation.')
+print(f'Assalamu Alaikum Wa RahmatUllah Hi Wa Barakatuhu, Respected, Mr.{Guest_list[1]}! I am very delighted to invite you on a dinner at my place, I would be really delighted if u accept my mere invitation.')
+print(f'Assalamu Alaikum Wa RahmatUllah Hi Wa Barakatuhu, Respected, Ms.{Guest_list[2]}! I am very delighted to invite you on a dinner at my place, I would be really delighted if u accept my mere invitation.')
+print(f'Respected, Mr.{Guest_list[3]}! I am very delighted to invite you on a dinner at my place, I would be really delighted if u accept my mere invitation.')
+print(f'Respected, Mr.{Guest_list[4]}! I am very delighted to invite you on a dinner at my place, I would be really delighted if u accept my mere invitation.')
+print(f'Respected, Ms.{Guest_list[5]}! I am very delighted to invite you on a dinner at my place, I would be really delighted if u accept my mere invitation.')
+print(f'Respected, Mr.{Guest_list[6]}! I am very delighted to invite you on a dinner at my place, I would be really delighted if u accept my mere invitation.')
+print('.')
+print('.')
+print('.')
+
+
+#Exercise 3-5: Changing Guest List
