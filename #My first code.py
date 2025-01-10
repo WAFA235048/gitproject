2934110d4dfe8bfbb7fc6747330821b2f88ce9d7
WAFA <wafadar801@gmail.com>
2025-01-10 15:45:10 +0500

    Added new Jupyter Notebook

---
+++ #My first code.py
@@ -0,0 +1,13 @@
+#My first code
+age:int=20
+print(age)
+height_1:float=2.6
+print(height_1)
+#string datatype
+myFullName:str='GOOD20'
+my_age:int=10
+print("My name is",myFullName,'and My age is:',my_age)
+print(f'My name is {myFullName} and my age is: {my_age}')
+
+
+
